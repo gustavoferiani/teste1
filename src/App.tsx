d43715232524d3ea1393cbe8ca209ac59a99a118
Luiz Otávio <luizomf@gmail.com>
2025-03-20 05:19:30 -0300

    Usando useState para o estado com programação reativa

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,27 @@ import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
 import { Heading } from './components/Heading';
+import { useState } from 'react';
 
 export function App() {
-  let numero = 0;
+  // Que todos os componentes que usam "numero"
+  // saibam das mudanças em seu valor.
+
+  // Sempre que eu usar useState, não vou usar atribuição diretamente
+  // const [numero, setNumero] = useState(() => {
+  //   console.log('Lazy initialization');
+  //   return 0;
+  // });
+  const [numero, setNumero] = useState(0);
 
   function handleClick() {
-    const span = document.getElementById('numero');
-
-    if (!span) return;
-
-    numero += 1;
-    span.innerText = numero.toString();
-    console.log(numero, Date.now());
+    // setNumero(prevState => prevState + 1);
+    setNumero(1);
   }
 
   return (
     <>
-      <Heading>
-        Número: <span id='numero'>{numero}</span>
-      </Heading>
+      <Heading>Número: {numero}</Heading>
       <button onClick={handleClick}>Aumenta</button>
 
       <Container>
