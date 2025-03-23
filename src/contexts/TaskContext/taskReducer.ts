9c5e5df5b5a1ec3c3241cef1011c73949c1101f7
Luiz Otávio <luizomf@gmail.com>
2025-03-23 05:34:00 -0300

    TypeScript + Reducer: criando actions bem tipadas e escaláveis?

---
+++ src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,22 @@
+import { TaskStateModel } from '../../models/TaskStateModel';
+import { TaskActionModel, TaskActionTypes } from './taskActions';
+
+export function taskReducer(
+  state: TaskStateModel,
+  action: TaskActionModel,
+): TaskStateModel {
+  switch (action.type) {
+    case TaskActionTypes.START_TASK: {
+      return state;
+    }
+    case TaskActionTypes.INTERRUPT_TASK: {
+      return state;
+    }
+    case TaskActionTypes.RESET_STATE: {
+      return state;
+    }
+  }
+
+  // Sempre deve retornar o estado
+  return state;
+}
